d2ce4411cba1f983425ae3390c1eadae1a9c7012
Hans <hans@Hanss-MacBook-Pro.local>
2017-04-10 19:11:00 -0700

    git clone working with gulp-exec but I can't loop through username list

--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,120 @@
+var $        	= require('gulp-load-plugins')();
+var argv     	= require('yargs').argv;
+var gulp     	= require('gulp');
+var rimraf   	= require('rimraf');
+var requireDir 	= require('require-dir');
+var router   	= require('front-router');
+var sequence 	= require('run-sequence');
+var fs 			= require('fs');
 
-var username = ["Cperez2187","World"];
-var reponame = "this";
+///////////////
+var shell 	 	= require('gulp-shell');
+var GulpSSH 	= require('gulp-ssh');
+var exec 		= require('gulp-exec');
+
+// Settings
 var site= "https://github.com/";
+// Add your student names here
+var username = [
+	"joselsalazar",
+	"Cperez2187",
+	"tkappha",
+	"Lolobrew",
+	"Cperez2187",
+	"afflatus480",
+	"cqliu1",
+	"jeffhatch",
+	"peques",
+	"wesvanduine"
+];
 
+// The naming should be standardized
+// Examples:
+// weeWeek1_Day2_html_css
+var reponame = "Basic-Portfolio";
 
+	// ssh config
+	var config = {
+	  host: '192.30.252.0', //'192.168.0.21',
+	  port: 22,
+	  username: 'node',
+	  privateKey: fs.readFileSync('/Users/hans/.ssh/id_rsa')
+	}
 
-// gulp.task('clone', () => {
-//   return gulp.src('*.js', {read: false})
-//   .pipe(shell([
-//     'echo <%= file.path %>'
-//   ]))
-// })
-// gulp.task('greet', shell.task('echo Hello, World!'))
+	var gulpSSH = new GulpSSH({
+	  ignoreErrors: false,
+	  sshConfig: config
+	})
 
+  var options = {
+    continueOnError: false, // default = false, true means don't emit error event 
+    pipeStdout: false, // default = false, true means stdout is written to file.contents 
+    customTemplatingThing: "test" // content passed to gutil.template() 
+  };
+  var reportOptions = {
+  	err: true, // default = true, false means don't write err 
+  	stderr: true, // default = true, false means don't write stderr 
+  	stdout: true // default = true, false means don't write stdout 
+  }
 
-
-// // Loop through a list of users and clone each repo
-// for (var i = 0; i 
-// 	< username.length; 
-// 	i++) 
-// {
-//   // "git clone " + /"https://github.com/" + username + "/" + reponame
-//     console.log(
-//    "git clone " + site + username[i] + "/" + reponame
-//     );
-// }
-
-
-// 1. LIBRARIES
+// 1. Clone repos
+// Create a task that loops through a list of users and clone each repo in a "build folder"
 // - - - - - - - - - - - - - - -
+ 
+	gulp.task('test', function() {
+		return gulp.src('./**/**')
+	    .pipe(exec('git clone '+  site + "/HansUXdev/repair-hub", options))
+	    // comment out the line below
+	    .pipe(exec.reporter(reportOptions));
+	});
 
-var $        = require('gulp-load-plugins')();
-var shell 	 = require('gulp-shell');
-var argv     = require('yargs').argv;
-var gulp     = require('gulp');
-var rimraf   = require('rimraf');
-var requireDir = require('require-dir');
-var router   = require('front-router');
-var sequence = require('run-sequence');
+	// gulp.task('shell', function () {
+	//   return gulpSSH
+	//     .shell(
+	//     	// 'git clone '+  site + "/HansUXdev/repair-hub", {filePath: 'shell.log'})
+	//     .pipe(gulp.dest('logs'))
+	// })
 
-// Check for --production flag
-var isProduction = !!(argv.production);
-
-// 2. FILE PATHS
-// - - - - - - - - - - - - - - -
-
-var paths = {
-  assets: [
-    './client/**/*.*',
-    '!./client/templates/**/*.*',
-    '!./client/assets/{scss,js}/**/*.*'
-  ]
-}
-
-// 3. TASKS
-// - - - - - - - - - - - - - - -
-
-// Cleans the build directory
-gulp.task('clean', function(cb) {
-  rimraf('./build', cb);
-});
-
-
-
-
-
-// for (var i = 0; i < username.length; i++) 
-// {
 	gulp.task('clone', shell.task(
 		// 'echo Hello, World!'
-		"git clone https://github.com/HansUXdev/TA-Helper"
-		   // "git clone " + site + username[i] + "/" + reponame
-
+		"git clone " +  site + "/HansUXdev/repair-hub"
+			// for (var i = 0; i < username.length; i++) 
+			// {
+		    // "git clone " + site + username[i] + "/" + reponame
+			// }
 	))
-// }
-// gulp.task('example', () => {
-//   return gulp.src('*.js', {read: false})
-//   .pipe(shell([
-//     'echo <%= file.path %>'
-//   ]))
-// })
+	
+	gulp.task('example', () => {
+	  return gulp.src('*.js', {read: false})
+	  .pipe(shell([
+	  	'git clone ' +  site + '/HansUXdev/repair-hub'
+	    // 'echo <%= file.path %>'
+	  ]))
+	})
+
+// 2. Lint the student files
+// Create a task that lints the students html, css and js
+// - - - - - - - - - - - - - - -
 
 
 
-// Starts a test server, which you can view at http://localhost:8079
-gulp.task('server', ['build'], function() {
-  gulp.src('./build')
-    .pipe($.webserver({
-      port: 8079,
-      host: 'localhost',
-      fallback: 'index.html',
-      livereload: true,
-      open: true
-    }))
-  ;
-});
+// 3. Server
+// Create a task to open up each students project in a new tab
+// - - - - - - - - - - - - - - -
+	// Starts a test server, which you can view at http://localhost:8079
+	gulp.task('server', ['build'], function() {
+		// for (var i = 0; i < username.length; i++)
+		gulp.src('./build')
+		.pipe($.webserver({
+			port: 8079,
+			host: 'localhost',
+			fallback: 'index.html',
+			livereload: true,
+			open: true
+		}))
+		;
+		// }
+	});
 
 
 
