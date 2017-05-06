e434b499818b025f8e7532e0dc4c5356d711d896
Hans <hans@Hanss-MacBook-Pro.local>
2017-05-06 09:21:12 -0700

    Rewrite for single command to clone all repos. Errors out with infinate loop saying /bin/sh: -c: line 1: syntax error: unexpected end of file

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,15 @@ var argv     	= require('yargs').argv;
 var gulp     	= require('gulp');
 var rimraf   	= require('rimraf');
 var requireDir 	= require('require-dir');
-var router   	= require('front-router');
 var sequence 	= require('run-sequence');
 var fs 			= require('fs');
 var shell 	 	= require('gulp-shell');
 var GulpSSH 	= require('gulp-ssh');
 var exec 		= require('gulp-exec');
+var csslint 	= require('gulp-csslint');
+var gutil 		= require('gulp-util');
+var jslint 		= require('gulp-jslint');
+	// var htmlReporter = require('gulp-csslint-report');
 
 // Settings
 var site= "https://github.com/";
@@ -16,27 +19,46 @@ var site= "https://github.com/";
 // This is just for testing
 var username= "/HansUXdev";
 
+// 
+// wesvanduine & kabrittan & heythisispaul
+
 // The naming should be standardized
 	// "quickDemoApp";
 	// "Basic-Portfolio";
-	// "/repair-hub";
-	var reponame = "/Basic-Portfolio";
-// 
+	// bootstrap-portfolio
+	// Hangman-Game
+	// Psychic-Game
+	// responsive-portfolio
+	var reponame = "/week-4-game";
+
 	var studentList = [
 		// "HansUXdev" // for testing
-		"peques",
-		"jeffhatch",
-		"joselsalazar",
-		"jeffhatch",
-		"Lolobrew",
-		"ptk88",
-		"wesvanduine",
-		"Cperez2187",
-		"tkappha",
-		"Cperez2187",
-		"afflatus480",
-		"cqliu1",
-		"heythisispaul",
+		/////// Hans's Students
+		"azcactus",	   	 	// Travis
+		'jalilakhtar',   	// Jalil
+		"nverges",		 	// Nick
+		"afflatus480", 	 	// Robert
+		"nathan25maloney",  // Nathan
+		"okaif90",			//
+		"ptk88",		 	// Phil
+		"peques", 		 	// Saul
+		"tkappha", 	   	 	// Tami
+		"heythisispaul", 	// Paul Richards //js
+		///////////////
+		// COAL
+		///////////////
+		"kabrittan",		// kyle
+		"wesvanduine", 	 	// Wes
+// nverges/Psychic-Game
+		// "peques",
+		// "jeffhatch",
+		// "joselsalazar",
+		// "jeffhatch",
+		// "Lolobrew",
+		// "Cperez2187",
+		// "afflatus480",
+		// "cqliu1",
+		// "heythisispaul",
 	];
 
   var options = {
@@ -54,10 +76,12 @@ var username= "/HansUXdev";
 // Create a task that loops through a list of users and clone each repo in a "build folder"
 // - - - - - - - - - - - - - - 
 	var urlList = [];
-	for (var i = 0; i < studentList.length; i++) {	
-		urlList[i] = 'mkdir '+studentList[i]+' && cd '+studentList[i]+ ' && git clone '+site+studentList[i]+reponame
-		// urlList[i] = 'mkdir ' 0+i++ +'cd ' i++ +'git clone '+site+studentList[i]+reponame
-		// console.log( 'git clone '+site+studentList[i]+reponame)
+	for (var i = 0; i < studentList.length; i++) {
+		// For new students add 'mkdir '+studentList[i]+ ' && cd '	
+		// clone individually
+			// urlList[i] = 'cd students/'+studentList[i]+ ' && git clone '+site+studentList[i]+reponame
+		// one giant command to clone them all
+			urlList[i] = 'git clone '+site+studentList[i]+reponame +" students/"+studentList[i]+reponame +" && "
 	};
 
 	gulp.task('clone', function() {
@@ -71,24 +95,58 @@ var username= "/HansUXdev";
 		.pipe(exec(urlList[5],options))
 		.pipe(exec(urlList[6],options))
 		.pipe(exec(urlList[7],options))
-		.pipe(exec(urlList[8],options))
-		.pipe(exec(urlList[9],options))
-		.pipe(exec(urlList[10],options))
-		.pipe(exec(urlList[11],options))
+		// .pipe(exec(urlList[8],options))
+		// .pipe(exec(urlList[9],options))
+		// .pipe(exec(urlList[10],options))
+		// .pipe(exec(urlList[11],options))
 		// .pipe(gulp.dest('./test'));
 	    // .pipe(exec(urlList[1],options))
 	    // comment out the line below
 	    .pipe(exec.reporter(reportOptions));
 	});
 
-// 2. Lint the student files
+// (). Add task to pull any changes for each repo
 // Create a task that loops through each student repo, lints the students html, css and js and exports a report
 // - - - - - - - - - - - - - - -
 
 
+// (). Lint the student files
+// Create a task that loops through each student repo, lints the students html, css and js and exports a report
+// - - - - - - - - - - - - - - -
 
 
-// 3. Server
+ 
+	gulp.task('jslint', function () {
+	    return gulp.src([studentList+''])
+	            .pipe(jslint({ /* this object represents the JSLint directives being passed down */ }))
+	            .pipe(jslint.reporter( 'my-reporter' ));
+	});
+
+	gulp.task('css', function() {
+	  gulp.src('joselsalazar'+reponame+'/assets/css/*.css')
+	    .pipe(csslint())
+	    // .pipe(htmlReporter());
+	});
+
+
+
+	 
+	// var customReporter = function(file) {
+	//   gutil.log(gutil.colors.cyan(file.csslint.errorCount)+' errors in '+gutil.colors.magenta(file.path));
+	 
+	//   file.csslint.results.forEach(function(result) {
+	//     gutil.log(result.error.message+' on line '+result.error.line);
+	//   });
+	// };
+	 
+	gulp.task('lint', function() {
+	  gulp.files('lib/*.css')
+	    .pipe(csslint())
+	    .pipe(csslint.reporter(customReporter));
+	});
+
+
+// (). Server
 // Create a task to open up each students project in a new tab
 // - - - - - - - - - - - - - - -
 
@@ -110,7 +168,7 @@ var username= "/HansUXdev";
 
 
 // Default task: builds your app, starts a server, and recompiles assets when they change
-gulp.task('default', ['server'], function () {
+gulp.task('default', ['s'], function () {
   
   // gulp.watch(['./client/**/*.*', '!./client/templates/**/*.*', '!./client/assets/{scss,js}/**/*.*'], ['copy']);
 
